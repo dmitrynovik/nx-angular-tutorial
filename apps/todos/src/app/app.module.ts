90ef730c1e53d07e66c5e34f2e7f90481534cf20
Dmitry Novik <dmitry.novik@gmail.com>
2020-03-04 15:27:26 +1100

    adding the UI library

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { UiModule } from '../../../../libs/ui/src/lib/ui.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule, 
     HttpClientModule,
+    UiModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   providers: [],
